917846fbe90dcea68270f0133322ceeb265fe00d
Taras <tarashuledza14@gmail.com>
2022-11-04 00:03:57 +0200

    test commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_5.1/Lab_5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = h(1, 1);
+			Assert::AreEqual(2, t,0.00001);
+		}
+	};
+}
